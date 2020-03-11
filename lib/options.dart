2192e4050f98a66ba6417a39d3ffd38172e0fcc5
Alex I <aiurovet@gmail.com>
2020-03-12 00:31:03 +1100

    Fixed config file, further enhanced detailed help

--- lib/options.dart
+++ lib/options.dart
@@ -182,7 +182,7 @@ DETAILS:
 
      grep -Pi "..." confdir/my.json | doul -c- -d projdir
 
-### Configuration file format
+### Configuration file format (see full sample file below)
 
 Originally, this tool was written to produce multiple icon files in PNG format
 from a single SVG source. The idea was that knowing width, height, input file
@@ -198,35 +198,25 @@ files from a single source template.
 Configuration file is expected in JSON format with the following guidelines:
 
 2.1. The name of the top node name can be anything, it does not matter.
+
 2.2. There should be two sub-nodes: associative array "rename" and an array
      of associative arrays "action".
+
 2.3. The sub-node "rename" should define case-sensitive translations for the
-     pre-defined placeholders:
-
-     "rename": {
-       "${Config.PARAM_NAME_COMMAND}": "{c}",
-       "${Config.PARAM_NAME_EXPAND_ENV}": "{EE}",
-       "${Config.PARAM_NAME_EXPAND_INP}": "{EI}",
-       "${Config.PARAM_NAME_HEIGHT}": "{h}",
-       "${Config.PARAM_NAME_INPUT}": "{i}",
-       "${Config.PARAM_NAME_OUTPUT}": "{o}",
-       "${Config.PARAM_NAME_TOPDIR}": "{TD}",
-       "${Config.PARAM_NAME_WIDTH}": "{w}"
-     },
-
-     Here the key is a pre-defined placeholder, and the value is the placeholder
-     to use instead. As you can see, this can make config file less verbose and
-     easier to read. These placeholders are self-descriptive, except the ones
-     containing \'expand\' in their name. The "${Config.PARAM_NAME_EXPAND_ENV}" means that
-     by setting its value to true or to false you allow or disallow expansion of
-     environment variables, and the "${Config.PARAM_NAME_EXPAND_INP}" means that the content of
-     the input file will also be expanded using pre-defined as well as user-
-     defined placeholders, and optionally, environment variables. Then it will
-     be saved to a temporary file, which will be used as an input for the sub-
-     sequent external command execution. All temporary files will be deleted on
-     the go. If no external command defined, then this will be interpreted as a
-     simple expansion of the input. However, in order to achieve that, the
-     "${Config.PARAM_NAME_EXPAND_INP}" flag is still required to be set to true.
+     pre-defined placeholders (see above). The key is a pre-defined placeholder,
+     and the value is the placeholder to use instead. As you can see, this can
+     make config file less verbose and easier to read. These placeholders are
+     self-descriptive, except the ones containing \'expand\' in their name. The
+     "{expand-environment}" means that by setting its value to true or to false
+     you allow or disallow expansion of environment variables, and the
+     "{expand-input}" means that the content of the input file will also be
+     expanded using pre-defined as well as user-defined placeholders, and
+     optionally, environment variables. Then it will be saved to a temporary
+     file, which will be used as an input for the sub-sequent external command
+     execution. All temporary files will be deleted on the go. If no external
+     command defined, then this will be interpreted as a simple expansion of the
+     input. However, in order to achieve that, the "{expand-input}" flag is
+     still required to be set to true.
 
 2.4. For the sake of source code portability, the environment variables are
      required to be specified strictly in UNIX/Linux/macOS format:
@@ -239,86 +229,102 @@ Configuration file is expected in JSON format with the following guidelines:
      it is also recommended (but not enforced) to avoid specifying DOS/Windows
      drive explicitly even if you run the program solely under those OSes.
 
-2.5. The sub-node "action" should define an array of associative arrays with the
+2.6. The sub-node "action" should define an array of associative arrays with the
      rest of missing information (please note that the next array overwrites
      whatever was defined before, thus only the last command is the actual one;
      on the other hand, you could switch it to another one later like in a sort
      of a batch):
 
-     "action": [
-       { "{EE}": true, "{EI}": true },
-
-       { "{c}": "wkhtmltoimage --format png \"{i}\" \"{o}\" # fails to display svg properly" },
-       { "{c}": "convert \"{i}\" \"{o}\" # not the best quality" },
-       { "{c}": "inkscape -z -e \"{o}\" -w {w} -h {h} \"{i}\" # not the best quality" },
-       { "{c}": "firefox --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # terribly slow" },
-       { "{c}": "google-chrome --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # the most accurate" },
-
-       { "{TD}": "\${HOME}\${USERPROFILE}/AndroidStudioProjects/my_flutter_app" },
-
-       { "{i}": "assets/images/app_bg.svg" },
-
-       { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_background.png" },
-       { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_background.png" },
-       { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_background.png" },
-       { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_background.png" },
-       { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_background.png" },
-
-       { "{i}": "assets/images/app_fg.svg" },
-
-       { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_foreground.png" },
-       { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_foreground.png" },
-       { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_foreground.png" },
-       { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_foreground.png" },
-       { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_foreground.png" },
-
-       { "{i}": "assets/images/app.svg" },
-
-       { "{w}":  48,  "{h}": 48, "{o}": "android/app/src/main/res/mipmap-mdpi/ic_launcher.png" },
-       { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/mipmap-hdpi/ic_launcher.png" },
-       { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/mipmap-xhdpi/ic_launcher.png" },
-       { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png" },
-       { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png" },
-
-       { "{w}": 1024, "{h}": 1024, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png" },
-       { "{w}": 20, "{h}": 20, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png" },
-       { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png" },
-       { "{w}": 60, "{h}": 60, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png" },
-       { "{w}": 29, "{h}": 29, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png" },
-       { "{w}": 58, "{h}": 58, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png" },
-       { "{w}": 87, "{h}": 87, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png" },
-       { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png" },
-       { "{w}": 80, "{h}": 80, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png" },
-       { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png" },
-       { "{w}": 50, "{h}": 50, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@1x.png" },
-       { "{w}": 100, "{h}": 100, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@2x.png" },
-       { "{w}": 57, "{h}": 57, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@1x.png" },
-       { "{w}": 114, "{h}": 114, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@2x.png" },
-       { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png" },
-       { "{w}": 180, "{h}": 180, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png" },
-       { "{w}": 72, "{h}": 72, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@1x.png" },
-       { "{w}": 144, "{h}": 144, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@2x.png" },
-       { "{w}": 76, "{h}": 76, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png" },
-       { "{w}": 152, "{h}": 152, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png" },
-       { "{w}": 167, "{h}": 167, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png" },
-
-       { "{w}": 192, "{h}": 192, "{o}": "web/icons/Icon-192.png" },
-       { "{w}": 512, "{h}": 512, "{o}": "web/icons/Icon-512.png" },
-
-       { "": null }
-     ]
-
-     The line with the empty key is totally unnecessary, as such keys will be
+2.7. The line with the empty key is totally unnecessary, as such keys will be
      ignored. However, it allows to end all previous lines with the comma, which
      is handy enough to utilise this approach.
 
-2.6. As you can see, any parameter can be changed at any time affecting sub-
+2.8. As you can see, any parameter can be changed at any time affecting sub-
      sequent data. 
 
-2.7. The given example is the one I used to produce multiple launcher icons for
-     a flutter app. Interestingly enough, it is forward-compatible with possible
-     new types of project, and a typical example of that would be the addition
-     of the last two data lines for web app generation
+2.9. The given sample file is the one I used to produce multiple launcher icons
+     for a flutter app. Interestingly enough, it is forward-compatible with
+     possible new types of project, and a typical example of that would be the
+     addition of the last two data lines for web app generation.
+
+### Full sample configuration file
+
+{
+  "x": {
+    "rename": {
+      "{command}": "{c}",
+      "{expand-environment}": "{EE}",
+      "{expand-input}": "{EI}",
+      "{height}": "{h}",
+      "{input}": "{i}",
+      "{output}": "{o}",
+      "{topDir}": "{TD}",
+      "{width}": "{w}"
+    },
+    "action": [
+      { "{EE}": true, "{EI}": true },
+
+      { "#{c}": "firefox --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # terribly slow" },
+      { "#{c}": "wkhtmltoimage --format png \"{i}\" \"{o}\" # fails to display svg properly" },
+      { "#{c}": "convert \"{i}\" \"{o}\" # not the best quality" },
+      { "#{c}": "inkscape -z -e \"{o}\" -w {w} -h {h} \"{i}\" # not the best quality" },
+      { "{c}": "google-chrome --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # the most accurate" },
+
+      { "{TD}": "\${HOME}\${USERPROFILE}/AndroidStudioProjects/back_your_school/" },
+
+      { "{i}": "assets/images/app_bg.svg" },
+
+      { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_background.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_background.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_background.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_background.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_background.png" },
+
+      { "{i}": "assets/images/app_fg.svg" },
+
+      { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_foreground.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_foreground.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_foreground.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_foreground.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_foreground.png" },
+
+      { "{i}": "assets/images/app.svg" },
+
+      { "{w}":  48,  "{h}": 48, "{o}": "android/app/src/main/res/mipmap-mdpi/ic_launcher.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/mipmap-hdpi/ic_launcher.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/mipmap-xhdpi/ic_launcher.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png" },
+
+      { "{w}": 1024, "{h}": 1024, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png" },
+      { "{w}": 20, "{h}": 20, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png" },
+      { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png" },
+      { "{w}": 60, "{h}": 60, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png" },
+      { "{w}": 29, "{h}": 29, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png" },
+      { "{w}": 58, "{h}": 58, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png" },
+      { "{w}": 87, "{h}": 87, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png" },
+      { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png" },
+      { "{w}": 80, "{h}": 80, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png" },
+      { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png" },
+      { "{w}": 50, "{h}": 50, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@1x.png" },
+      { "{w}": 100, "{h}": 100, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@2x.png" },
+      { "{w}": 57, "{h}": 57, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@1x.png" },
+      { "{w}": 114, "{h}": 114, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@2x.png" },
+      { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png" },
+      { "{w}": 180, "{h}": 180, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png" },
+      { "{w}": 72, "{h}": 72, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@1x.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@2x.png" },
+      { "{w}": 76, "{h}": 76, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png" },
+      { "{w}": 152, "{h}": 152, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png" },
+      { "{w}": 167, "{h}": 167, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png" },
+
+      { "{w}": 192, "{h}": 192, "{o}": "web/icons/Icon-192.png" },
+      { "{w}": 512, "{h}": 512, "{o}": "web/icons/Icon-512.png" },
+
+      { "": null }
+    ]
+  }
+}
 ''');
     }
 
