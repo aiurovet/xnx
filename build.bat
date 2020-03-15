974b691a4d59a95ec9f6606c1afdc7fe6ffb0167
Alex I <aiurovet@gmail.com>
2020-03-16 02:09:51 +1100

    Built Windows app under Linux

--- build.bat
+++ build.bat
@@ -1,10 +1,15 @@
 @echo off
-set BIN=%~dp0\bin
 
-pub get
+%~d0
 if errorlevel 1 exit /B 1
 
-dart2native "%BIN%\doul.dart" -o "%BIN%\windows\doul.exe"
+cd %~dp0
+if errorlevel 1 exit /B 1
+
+call pub get
+if errorlevel 1 exit /B 1
+
+call dart2native "bin\doul.dart" -o "bin\windows\doul.exe"
 if errorlevel 1 exit /B 1
 
 exit /B 0
