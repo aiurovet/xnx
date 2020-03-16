cf934629036521cc2fe3e156e5f17d2ac1d080b8
Alex I <aiurovet@gmail.com>
2020-03-17 00:19:15 +1100

    Improved docos, renamed data files for better clarity

--- lib/options.dart
+++ lib/options.dart
@@ -266,31 +266,40 @@ Configuration file is expected in JSON format with the following guidelines:
      input. However, in order to achieve that, the "{expand-input}" flag is
      still required to be set to true.
 
-2.4. For the sake of source code portability, the environment variables are
+2.4. To support \'expand-input\' feature, the program creates a temporary file
+     where all placeholders get expanded. This file is located in the same
+     directory where the current output file is supposed to be as well as
+     with the same name, but the extension is replaced with .tmp.<input-ext>.
+     For instance, if assets/images/custom.svg is converted to
+     android/app/src/main/res/drawable-xhdpi/ic_launcher_background.png, then
+     the temporary file path will be 
+     android/app/src/main/res/drawable-xhdpi/ic_launcher_background.tmp.svg
+
+2.5. For the sake of source code portability, the environment variables are
      required to be specified strictly in UNIX/Linux/macOS format:
      \$ABC_123_DEF4 or \${ABC_123_DEF4} (under Windows though, environment
      variables will be considered case-insensitive). You can escape expansion
      by doubling the dollar sign: \$\$ABC.
 
-2.5. Directory separator char in file paths is also required to be specified in
+2.6. Directory separator char in file paths is also required to be specified in
      UNIX/Linux/macOS style: "abc/def/xyz.svg". For the sake of code portability
      it is also recommended (but not enforced) to avoid specifying DOS/Windows
      drive explicitly even if you run the program solely under those OSes.
 
-2.6. The sub-node "action" should define an array of associative arrays with the
+2.7. The sub-node "action" should define an array of associative arrays with the
      rest of missing information (please note that the next array overwrites
      whatever was defined before, thus only the last command is the actual one;
      on the other hand, you could switch it to another one later like in a sort
      of a batch):
 
-2.7. The line with the empty key is totally unnecessary, as such keys will be
+2.8. The line with the empty key is totally unnecessary, as such keys will be
      ignored. However, it allows to end all previous lines with the comma, which
      is handy enough to utilise this approach.
 
-2.8. As you can see, any parameter can be changed at any time affecting sub-
+2.9. As you can see, any parameter can be changed at any time affecting sub-
      sequent data. 
 
-2.9. The given sample file is the one I used to produce multiple launcher icons
+2.10.The given sample file is the one I used to produce multiple launcher icons
      for a flutter app. Interestingly enough, it is forward-compatible with
      possible new types of project, and a typical example of that would be the
      addition of the last two data lines for web app generation.
@@ -316,58 +325,58 @@ Configuration file is expected in JSON format with the following guidelines:
       { "#{c}": "wkhtmltoimage --format png \"{i}\" \"{o}\" # fails to display svg properly" },
       { "#{c}": "convert \"{i}\" \"{o}\" # not the best quality" },
       { "#{c}": "inkscape -z -e \"{o}\" -w {w} -h {h} \"{i}\" # not the best quality" },
-      { "{c}": "google-chrome --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # the most accurate" },
+      { "{c}": "chrome --headless --default-background-color=0 --window-size={w},{h} --screenshot=\"{o}\" \"file://{i}\" # the most accurate" },
 
-      { "{CD}": "back_your_school/" },
+      { "{CD}": "flutter_app", "{img-src-dir}": "{CD}/assets/images" },
 
-      { "{i}": "assets/images/app_bg.svg" },
+      { "{i}": "{img-src-dir}/app_bg.svg" },
 
-      { "{w}":   48, "{h}":   48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_background.png" },
-      { "{w}":   72, "{h}":   72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_background.png" },
-      { "{w}":   96, "{h}":   96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_background.png" },
-      { "{w}":  144, "{h}":  144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_background.png" },
-      { "{w}":  192, "{h}":  192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_background.png" },
+      { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_background.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_background.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_background.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_background.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_background.png" },
 
-      { "{i}": "assets/images/app_fg.svg" },
+      { "{i}": "{img-src-dir}/app_fg.svg" },
 
-      { "{w}":   48, "{h}":   48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_foreground.png" },
-      { "{w}":   72, "{h}":   72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_foreground.png" },
-      { "{w}":   96, "{h}":   96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_foreground.png" },
-      { "{w}":  144, "{h}":  144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_foreground.png" },
-      { "{w}":  192, "{h}":  192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_foreground.png" },
+      { "{w}":  48, "{h}":  48, "{o}": "android/app/src/main/res/drawable-mdpi/ic_launcher_foreground.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/drawable-hdpi/ic_launcher_foreground.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/drawable-xhdpi/ic_launcher_foreground.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/drawable-xxhdpi/ic_launcher_foreground.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/drawable-xxxhdpi/ic_launcher_foreground.png" },
 
-      { "{i}": "assets/images/app.svg" },
+      { "{i}": "{img-src-dir}/app.svg" },
 
-      { "{w}":   48, "{h}":   48, "{o}": "android/app/src/main/res/mipmap-mdpi/ic_launcher.png" },
-      { "{w}":   72, "{h}":   72, "{o}": "android/app/src/main/res/mipmap-hdpi/ic_launcher.png" },
-      { "{w}":   96, "{h}":   96, "{o}": "android/app/src/main/res/mipmap-xhdpi/ic_launcher.png" },
-      { "{w}":  144, "{h}":  144, "{o}": "android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png" },
-      { "{w}":  192, "{h}":  192, "{o}": "android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png" },
+      { "{w}":  48,  "{h}": 48, "{o}": "android/app/src/main/res/mipmap-mdpi/ic_launcher.png" },
+      { "{w}":  72, "{h}":  72, "{o}": "android/app/src/main/res/mipmap-hdpi/ic_launcher.png" },
+      { "{w}":  96, "{h}":  96, "{o}": "android/app/src/main/res/mipmap-xhdpi/ic_launcher.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png" },
 
       { "{w}": 1024, "{h}": 1024, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png" },
-      { "{w}":   20, "{h}":   20, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png" },
-      { "{w}":   40, "{h}":   40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png" },
-      { "{w}":   60, "{h}":   60, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png" },
-      { "{w}":   29, "{h}":   29, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png" },
-      { "{w}":   58, "{h}":   58, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png" },
-      { "{w}":   87, "{h}":   87, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png" },
-      { "{w}":   40, "{h}":   40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png" },
-      { "{w}":   80, "{h}":   80, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png" },
-      { "{w}":  120, "{h}":  120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png" },
-      { "{w}":   50, "{h}":   50, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@1x.png" },
-      { "{w}":  100, "{h}":  100, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@2x.png" },
-      { "{w}":   57, "{h}":   57, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@1x.png" },
-      { "{w}":  114, "{h}":  114, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@2x.png" },
-      { "{w}":  120, "{h}":  120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png" },
-      { "{w}":  180, "{h}":  180, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png" },
-      { "{w}":   72, "{h}":   72, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@1x.png" },
-      { "{w}":  144, "{h}":  144, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@2x.png" },
-      { "{w}":   76, "{h}":   76, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png" },
-      { "{w}":  152, "{h}":  152, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png" },
-      { "{w}":  167, "{h}":  167, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png" },
+      { "{w}": 20, "{h}": 20, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png" },
+      { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png" },
+      { "{w}": 60, "{h}": 60, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png" },
+      { "{w}": 29, "{h}": 29, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png" },
+      { "{w}": 58, "{h}": 58, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png" },
+      { "{w}": 87, "{h}": 87, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png" },
+      { "{w}": 40, "{h}": 40, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png" },
+      { "{w}": 80, "{h}": 80, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png" },
+      { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png" },
+      { "{w}": 50, "{h}": 50, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@1x.png" },
+      { "{w}": 100, "{h}": 100, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-50x50@2x.png" },
+      { "{w}": 57, "{h}": 57, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@1x.png" },
+      { "{w}": 114, "{h}": 114, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-57x57@2x.png" },
+      { "{w}": 120, "{h}": 120, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png" },
+      { "{w}": 180, "{h}": 180, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png" },
+      { "{w}": 72, "{h}": 72, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@1x.png" },
+      { "{w}": 144, "{h}": 144, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-72x72@2x.png" },
+      { "{w}": 76, "{h}": 76, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png" },
+      { "{w}": 152, "{h}": 152, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png" },
+      { "{w}": 167, "{h}": 167, "{o}": "ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png" },
 
-      { "{w}":  192, "{h}":  192, "{o}": "web/icons/Icon-192.png" },
-      { "{w}":  512, "{h}":  512, "{o}": "web/icons/Icon-512.png" },
+      { "{w}": 192, "{h}": 192, "{o}": "web/icons/Icon-192.png" },
+      { "{w}": 512, "{h}": 512, "{o}": "web/icons/Icon-512.png" },
 
       { "": null }
     ]
