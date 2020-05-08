c0278120b0c883ac58ff01c842cf5023225232d9
Alex I <aiurovet@gmail.com>
2020-05-08 20:04:26 +1000

    WIP

--- lib/file_oper.dart
+++ lib/file_oper.dart
@@ -1,22 +1,45 @@
 import 'dart:io';
+import 'package:doul/ext/string.dart';
 import 'package:path/path.dart' as Path;
 
 import 'ext/glob.dart';
 
 class FileOper {
 
-  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
-  static void copySync(String fromPath, String toPath, {bool isMove, bool isNewerOnly}) {
-    // Ensuring flags
+  static void copySync(String fromPath, String toPath, {bool move = false, bool newerOnly = false}) {
+    var isToFile = File(toPath).existsSync();
 
-    isMove ??= false;
-    isNewerOnly ??= false;
+    listSync(fromPath, repeats: 2, filesFirst: true, suppressErrors: false, listProc: (entities, entityNo, repeatNo, subPath) {
+      if (entities.isEmpty) {
+        throw Exception('Source was not found: "${fromPath}"');
+      }
+
+      var entity = entities[entityNo];
+      var fromPathEx = entity.path;
+      var toDirName = (isToFile ? null : Path.join(toPath, subPath));
+      var toPathEx = (isToFile ? toPath : Path.join(toDirName, Path.basename(fromPathEx)));
+
+      if ((repeatNo == 0) && (entity is File)) {
+        copyFileSync(fromPathEx, toPathEx, move: move, newerOnly: newerOnly);
+        return true;
+      }
+      else if ((repeatNo == 1) && (entity is Directory)) {
+        if (!entity.existsSync()) {
+          if (!move) {
+            createDirectorySync(toDirName);
+          }
+        }
+      }
+
+      return true;
+    });
 
     // If source is existing file, then just copy it
 
     if (File(fromPath).existsSync()) {
-      copyFileSync(fromPath, toPath, isMove: isMove, isNewerOnly: isNewerOnly);
+      copyFileSync(fromPath, toPath, move: move, newerOnly: newerOnly);
       return;
     }
 
@@ -63,13 +86,13 @@ class FileOper {
       }
 
       if (fromEntity is File) {
-        copyFileSync(fromPath, toDirName, isMove: isMove, isNewerOnly: isNewerOnly);
+        copyFileSync(fromPath, toDirName, move: move, newerOnly: newerOnly);
       }
       else if ((fromEntity is Directory)) {
         if (fromPath == toDirName) {
-          throw Exception('Cannot ${isMove ?? 0 ? 'move': 'copy'} directory onto itself: "${fromPath}"');
+          throw Exception('Cannot ${move ?? 0 ? 'move': 'copy'} directory onto itself: "${fromPath}"');
         }
-        if (!isMove) {
+        if (!move) {
           createDirectorySync(toDirName);
         }
       }
@@ -77,7 +100,7 @@ class FileOper {
 
     // Delete source directory in case of move and all files have been moved
 
-    if (isMove) {
+    if (move) {
       var fromDir = Directory(fromDirName);
       var hasFiles = fromDir.listSync(recursive: true).any((x) => (x is File));
 
@@ -87,14 +110,9 @@ class FileOper {
     }
   }
 
-  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
-  static void copyFileSync(String fromPath, String toPath, {bool isMove, bool isNewerOnly}) {
-    // Ensuring flags
-
-    isMove ??= false;
-    isNewerOnly ??= false;
-
+  static void copyFileSync(String fromPath, String toPath, {bool move = false, bool newerOnly = false}) {
     // Ensuring source file exists
 
     var fromFile = File(fromPath);
@@ -114,7 +132,7 @@ class FileOper {
 
     // Setting operation flag depending on whether the destination is newer or not
 
-    if (isNewerOnly) {
+    if (newerOnly) {
       var toFile = File(toPathEx);
       var toStat = toFile.statSync();
 
@@ -131,7 +149,7 @@ class FileOper {
       createDirectorySync(toDirName);
     }
 
-    if (isMove) {
+    if (move) {
       if (canDo) {
         fromFile.renameSync(toPathEx);
       }
@@ -144,7 +162,7 @@ class FileOper {
     }
   }
 
-  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
   static void createDirectorySync(String dirName, {bool recursive = true}) {
     var dir = Directory(dirName);
@@ -154,7 +172,7 @@ class FileOper {
     }
   }
 
-  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
 
   static void deleteDirectorySync(String dirName) {
     var dir = Directory(dirName);
@@ -164,6 +182,151 @@ class FileOper {
     }
   }
 
-  ////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  static List<FileSystemEntity> listSync(
+      String path, {
+      int repeats = 1,
+      bool sorted = true,
+      bool filesFirst = false,
+      bool suppressErrors = false,
+      bool Function(List<FileSystemEntity> entities, int entityNo, int repeatNo, String subPath) listProc,
+      int Function(FileSystemEntity entity1, FileSystemEntity entity2) sortProc
+    }) {
+    // Get list of file system entities to walk through
+
+    var dirName = path;
+    var dirNameLen = dirName.length;
+
+    var entities = <FileSystemEntity>[];
+
+    var dir = Directory(dirName);
+
+    // If path is an existing directory, then grab all of it's children
+
+    if (dir.existsSync()) {
+      ++dirNameLen;
+      path = StringExt.EMPTY;
+      entities = dir.listSync(recursive: true);
+    }
+    else {
+      var file = File(path);
+
+      // If path is an existing file, then the list is just a single file
+
+      if (file.existsSync()) {
+        dirName = Path.dirname(path);
+        dirNameLen = dirName.length + 1;
+        path = path.substring(dirNameLen);
+        entities.add(file);
+      }
+      else {
+        // If we've got here, then either path contains wildcard(s), or it
+        // simply does not exist
+
+        dirName = GlobExt.getDirectoryName(path);
+
+        // If source directory does not exist, ensure the parent directory exists
+
+        dir = Directory(dirName);
+
+        if (!dir.existsSync()) {
+          if (suppressErrors ?? false) {
+            return null;
+          }
+          else {
+            throw Exception('Top source directory was not found: "${dirName}"');
+          }
+        }
+
+        dirNameLen = dirName.length + 1;
+        path = path.substring(dirNameLen);
+
+        // Get the list of all files and directories matching path pattern
+
+        var filter = GlobExt.toGlob(path);
+        entities = filter.listSync(root: dirName);
+      }
+    }
+
+    if ((sorted ?? false) && (entities.length > 1)) {
+      sort(entities, filesFirst: (filesFirst ?? false), sortProc: sortProc);
+    }
+
+    if (listProc != null) {
+      for (var repeatNo = 0; repeatNo < repeats; repeatNo++) {
+        var entityCount = entities.length;
+
+        if (entityCount > 0) {
+          for (var entityNo = 0; entityNo < entityCount; entityNo++) {
+            var subPath = entities[entityNo].path.substring(dirNameLen);
+
+            if (!listProc(entities, entityNo, repeatNo, subPath)) {
+              break;
+            }
+          }
+        }
+        else {
+          listProc(entities, -1, -1, null);
+        }
+      }
+
+      entities = null;
+    }
+
+    return entities;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  static void sort(
+      List<FileSystemEntity> entities, {
+      bool filesFirst = false,
+      int Function(FileSystemEntity entity1, FileSystemEntity entity2) sortProc
+    }) {
+    var pathSep = Platform.pathSeparator;
+
+    if (sortProc != null) {
+      entities.sort(sortProc);
+    }
+    else {
+      entities.sort((e1, e2) {
+        var result = 0;
+
+        var isDir1 = (e1 is Directory);
+        var isDir2 = (e2 is Directory);
+
+        if (isDir1 == isDir2) {
+          var pathComps1 = e1.path.split(pathSep);
+          var pathComps2 = e2.path.split(pathSep);
+
+          var pathCompCount1 = pathComps1.length;
+          var pathCompCount2 = pathComps2.length;
+          var pathCompCountMin = (pathCompCount1 < pathCompCount2
+              ? pathCompCount1
+              : pathCompCount2);
+
+          for (var i = 0; (result == 0) && (i < pathCompCountMin); i++) {
+            result = pathComps1[i].compareTo(pathComps2[i]);
+          }
+
+          if (result == 0) {
+            result = (pathCompCount1 < pathCompCount2 ? -1 : (pathCompCount1 >
+                pathCompCount2 ? 1 : 0));
+          }
+        }
+        else if (filesFirst) {
+          result = (isDir1 && !isDir2 ? 1 : -1);
+        }
+        else {
+          result = (isDir1 && !isDir2 ? -1 : 1);
+        }
+
+        return result;
+      });
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
 
 }
