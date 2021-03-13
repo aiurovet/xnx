3dae9a7668bb7798d60f794b387a423619687d6f
Alex I <aiurovet@gmail.com>
2021-03-13 13:03:39 +1100

    Converted CI yml to the use of build scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,7 +3,7 @@
 set PRJ=doul
 set BIN=bin
 set OST=Windows
-set BIN_OS=%BIN%\%OST%
+set BIN_OST=%BIN%\%OST%
 
 %~d0
 if errorlevel 1 exit /B 1
@@ -14,14 +14,14 @@ if errorlevel 1 exit /B 1
 call pub get
 if errorlevel 1 exit /B 1
 
-call dart2native "%BIN%\main.dart" -o "%BIN_OS%\%PRJ%.exe"
+call dart2native "%BIN%\main.dart" -o "%BIN_OST%\%PRJ%.exe"
 if errorlevel 1 exit /B 1
 
 set OSTYPE=%OST%
 call scripts\mkicons\mkicons
 if errorlevel 1 exit /B 1
 
-copy /Y "README.md" "%BIN_OS%"
+copy /Y "README.md" "%BIN_OST%"
 if errorlevel 1 exit /B 1
 
 exit /B 0
