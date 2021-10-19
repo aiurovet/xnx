2d3460acb1e09d07fbcf2e0e6c3666e400d2bf6d
aiurovet <aiurovet@gmail.com>
2021-10-19 21:06:19 +1100

    WIP

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,8 +3,8 @@
 setlocal EnableDelayedExpansion
 
 set PRJ=xnx
-set OST=Windows
-set ZIP=app\%PRJ%-windows.zip
+set PKG=app\%PRJ%-windows.zip
+set EXE=bin\%PRJ%.exe
 
 %~d0
 if errorlevel 1 exit /B 1
@@ -12,12 +12,12 @@ if errorlevel 1 exit /B 1
 cd "%~dp0.."
 if errorlevel 1 exit /B 1
 
-set OPT_MOVE=1
+set OPT_KEEP=0
 
 :loop
 
 if /I "%~1" == "/K" (
-    set OPT_MOVE=0
+    set OPT_KEEP=1
 ) else (
     set ARGS=!ARGS! %1
 )
@@ -28,32 +28,47 @@ if "%~1" neq "" goto :loop
 rem Reset errorlevel
 ver > nul
 
+echo Running the build for Windows
+
 if not exist out (
+    echo Creating the output directory
     mkdir out
     if errorlevel 1 exit /B 1
 )
 
+echo Getting the latest version of the packages
 call dart pub get
 if errorlevel 1 exit /B 1
 
-dart compile exe "bin\main.dart" -o "out\%PRJ%.exe"
+echo Compiling "%EXE%"
+dart compile exe bin\main.dart -o "%EXE%"
 if errorlevel 1 exit /B 1
 
-set OSTYPE=%OST%
-call scripts\mkicons\mkicons !ARGS!
+echo Copying the executable to the output directory
+xcopy /Q "%EXE%" out
 if errorlevel 1 exit /B 1
 
-xcopy /I /Q /S examples out\examples
-if errorlevel 1 exit /B 1
-
+echo Copying README to the output directory
 xcopy /Q README.md out
 if errorlevel 1 exit /B 1
 
-out\%PRJ% --zip out "%ZIP%"
+echo Copying examples to the output directory
+xcopy /I /Q /S examples out\examples
 if errorlevel 1 exit /B 1
 
-if %OPT_MOVE% neq 0 (
-    rmdir /Q /S out
+echo Creating the icons in the output directory
+%EXE% -d scripts\mkicons !ARGS!
+if errorlevel 1 exit /B 1
+
+echo Creating the application package
+"%EXE%" --move --zip out "%PKG%"
+if errorlevel 1 exit /B 1
+
+dir "%PKG%"
+
+if %OPT_KEEP% equ 0 (
+    echo Removing the output directory
+    del /Q "%EXE%"
 )
 
 exit /B 0
