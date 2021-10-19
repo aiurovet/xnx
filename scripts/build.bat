068635634053bf43852385685bfd52751674faa8
aiurovet <aiurovet@gmail.com>
2021-10-19 15:42:00 +1100

    Simplifying the build

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,19 +3,20 @@
 setlocal EnableDelayedExpansion
 
 set PRJ=xnx
-set APP=app
-set BIN=bin
 set OST=Windows
-set OST_LC=windows
+set ZIP=app\%PRJ%-windows.zip
 
-set ZIP=%APP%\%PRJ%-%OST_LC%.zip
-set BIN_OST=%BIN%\%OST%\%PRJ%
+%~d0
+if errorlevel 1 exit /B 1
+
+cd "%~dp0.."
+if errorlevel 1 exit /B 1
 
 set OPT_MOVE=1
 
 :loop
 
-if "%~1" == "-k" (
+if /I "%~1" == "/K" (
     set OPT_MOVE=0
 ) else (
     set ARGS=!ARGS! %1
@@ -27,38 +28,32 @@ if "%~1" neq "" goto :loop
 rem Reset errorlevel
 ver > nul
 
-%~d0
-if errorlevel 1 exit /B 1
-
-cd %~dp0
-if errorlevel 1 exit /B 1
-
-if not exist "%BIN_OST%" (
-    mkdir "%BIN_OST%"
+if not exist out (
+    mkdir out
     if errorlevel 1 exit /B 1
 )
 
 call dart pub get
 if errorlevel 1 exit /B 1
 
-dart compile exe "%BIN%\main.dart" -o "%BIN_OST%\%PRJ%.exe"
+dart compile exe "bin\main.dart" -o "out\%PRJ%.exe"
 if errorlevel 1 exit /B 1
 
 set OSTYPE=%OST%
 call scripts\mkicons\mkicons !ARGS!
 if errorlevel 1 exit /B 1
 
-xcopy /I /Q /S examples "%BIN_OST%/examples"
+xcopy /I /Q /S examples out\examples
 if errorlevel 1 exit /B 1
 
-xcopy /Q "README.md" "%BIN_OST%"
+xcopy /Q README.md out
 if errorlevel 1 exit /B 1
 
-"%BIN_OST%\%PRJ%" --zip "%BIN_OST%" "%ZIP%"
+out\%PRJ% --zip out "%ZIP%"
 if errorlevel 1 exit /B 1
 
 if %OPT_MOVE% neq 0 (
-    rmdir /Q /S "%BIN%\%OST%"
+    rmdir /Q /S out
 )
 
 exit /B 0
--- scripts/mkicons/mkicons.bat
+++ scripts/mkicons/mkicons.bat
@@ -9,18 +9,9 @@ if errorlevel 1 exit /B 1
 cd %~dp0..\..
 if errorlevel 1 exit /B 1
 
-set PRJ_NAME=xnx
-set PRJ_EXE=bin\Windows\%PRJ_NAME%\%PRJ_NAME%.exe
+set PRJ=xnx
 
-if not exist "%PRJ_EXE%" (
-  echo Executable file "%PRJ_EXE%" not found. Performing build.
-  call scripts\build.bat
-  if errorlevel 1 goto FAILURE
-)
-
-if not exist "%PRJ_EXE%" goto FAILURE
-
-"%PRJ_EXE%" -d scripts\mkicons %*
+"out\%PRJ%" -d scripts\mkicons %*
 if errorlevel 1 goto FAILURE
 
 @echo off
