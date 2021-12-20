74707de0cf8a49e156c07e707a8eabddd9e9aa30
aiurovet <aiurovet@gmail.com>
2021-12-21 00:03:18 +1100

    Finalise Check-FileCount

--- scripts/Check-FileCount.ps1
+++ scripts/Check-FileCount.ps1
@@ -10,20 +10,18 @@ Param(
   [Parameter(Mandatory=$false)] [String] $Exclude = '',
   [Parameter(Mandatory=$false)] [Int32] $Depth = [System.Int32]::MaxValue,
   [Parameter(Mandatory=$false)] [Int32] $Expected = [System.Int32]::MinValue,
+  [Switch] $Help,
+  [Switch] $Quiet,
   [Switch] $Recurse
 )
 
 ################################################################################
-# Adjust command-line parameters
+# Adjust command-line parameters and set internal parameters
 ################################################################################
 
 If (!$Recurse) {
   $Depth = 0
 }
-
-################################################################################
-# Set internal parameters
-################################################################################
 
 $ScriptName = [System.IO.Path]::GetFileName($PSCommandPath)
 
@@ -31,9 +29,33 @@ $ScriptName = [System.IO.Path]::GetFileName($PSCommandPath)
 # Check command-line parameters and print usage if failed
 ################################################################################
 
-If ($Path.Length -le 0) {
-  Write-Host "`nUSAGE: $ScriptName -Path <comma-separated-list> [-Recurse] [-Depth <number>] [-Include <mask>] [-Exclude <mask>] [-Expected <number>]`n"
+If (($Help) || ($Path.Length -le 0)) {
+  Write-Host @"
+
+USAGE:
+
+$ScriptName [OPTIONS]
+
+OPTIONS:
+
+-Help              - this help screen
+-Quiet             - do not print anything
+-Recurse           - scan all sub-directories (max depth)
+-Path <list>       - a comma-separated list of directories and/or files (the wildcards are allowed)
+-Depth <number>    - how deep the directory tree should be scanned (default: 0 in the non-recursive mode)
+-Include <mask>    - fetch only those filenames (the wildcards are allowed)
+-Exclude <mask>    - skip those filenames (the wildcards are allowed)
+-Expected <number> - succeed only if the number of directories and/or files equals this one (default: don't check)
+
+"@
+
   Exit 1
+}
+
+################################################################################
+
+if (!$Quiet) {
+  Write-Host "`n$ScriptName started`n";
 }
 
 ################################################################################
@@ -42,12 +64,24 @@ $Actual = (Get-ChildItem -Path "$Path" -Include "$Include" -Exclude "$Exclude" -
 $LastExitCode = If ($?) { 0 } Else { 1 }
 
 If ($Expected -le [System.Int32]::MinValue) {
-  $Actual | Out-Host;
+  if (!$Quiet) {
+    Write-Output "Actual: $Actual";
+  }
 }
 Else {
   If ($Actual -ne $Expected) {
     $LastExitCode = 1
   }
+
+  $Result = If ($LastExitCode -eq 0) { "OK" } Else { "Failed" }
+
+  if (!$Quiet) {
+    Write-Output "Actual: $Actual`nExpected: $Expected`nResult: $Result";
+  }
+}
+
+if (!$Quiet) {
+  Write-Host "`n$ScriptName completed`n";
 }
 
 Exit $LastExitCode
