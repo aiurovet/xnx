274eb3b3c475e2efb9f5c03a2fe94ca77e11a31a
aiurovet <aiurovet@gmail.com>
2021-12-20 23:07:31 +1100

    Testing PowerShell on Linux

--- scripts/Check-FileCount.ps1
+++ scripts/Check-FileCount.ps1
@@ -1,24 +1,54 @@
 #!/usr/bin/env pwsh
 
 ################################################################################
+# Define command-line parameters
+################################################################################
 
-if ($Args.Count -le 1)
-{
-    Write-Host "`nUSAGE: Check-FileCount <expected-count> <files1> [<files2>...]`n";
-    Exit 1
+Param(
+  [Parameter(Mandatory=$false)] [String] $Path = '',
+  [Parameter(Mandatory=$false)] [String] $Include = '',
+  [Parameter(Mandatory=$false)] [String] $Exclude = '',
+  [Parameter(Mandatory=$false)] [Int32] $Depth = [System.Int32]::MaxValue,
+  [Parameter(Mandatory=$false)] [Int32] $Expected = [System.Int32]::MinValue,
+  [Switch] $Recurse
+)
+
+################################################################################
+# Adjust command-line parameters
+################################################################################
+
+If (!$Recurse) {
+  $Depth = 0
+}
+
+################################################################################
+# Set internal parameters
+################################################################################
+
+$ScriptName = [System.IO.Path]::GetFileName($PSCommandPath)
+
+################################################################################
+# Check command-line parameters and print usage if failed
+################################################################################
+
+If ($Path.Length -le 0) {
+  Write-Host "`nUSAGE: $ScriptName -Path <comma-separated-list> [-Recurse] [-Depth <number>] [-Include <mask>] [-Exclude <mask>] [-Expected <number>]`n"
+  Exit 1
 }
 
 ################################################################################
 
-$ExpectedCount = $Args[0];
-$FileMasks = $Args[1..$Args.Count];
+$Actual = (Get-ChildItem -Path "$Path" -Include "$Include" -Exclude "$Exclude" -Depth $Depth).Count;
+$LastExitCode = If ($?) { 0 } Else { 1 }
 
-################################################################################
-
-(Get-ChildItem -Path $FileMasks).Count |`
-  Select-String -Quiet -Pattern "^[ \t]*$ExpectedCount[ \t]*$"
-
-################################################################################
+If ($Expected -le [System.Int32]::MinValue) {
+  $Actual | Out-Host;
+}
+Else {
+  If ($Actual -ne $Expected) {
+    $LastExitCode = 1
+  }
+}
 
 Exit $LastExitCode
 
