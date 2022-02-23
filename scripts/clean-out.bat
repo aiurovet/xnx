eda7eb6a634a46acf2916770ff6a0708040e5d28
aiurovet <aiurovet@gmail.com>
2022-02-24 00:21:16 +1100

    Changed the default value of cmdEscape and added clean-out scripts

--- scripts/clean-out.bat
+++ scripts/clean-out.bat
@@ -6,62 +6,65 @@
 
 @echo Switching to the project's top directory
 
+pushd
+
 %~d0
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 cd "%~dp0.."
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
-TOP=%CD%
+set TOP=%CD%
 
 @rem ***************************************************************************
 
 cd "%TOP%\examples\flutter_app_icons"
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 echo Cleaning in: %CD%
 rmdir /S /Q android ios web
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 @rem ***************************************************************************
 
 cd "%TOP%\examples\ms_office"
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 echo Cleaning in: %CD%
 rmdir /S /Q out
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 @rem ***************************************************************************
 
 cd "%TOP%\examples\multi_conf"
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 echo Cleaning in: %CD%
 rmdir /S /Q out
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 @rem ***************************************************************************
 
 cd "%TOP%\examples\site_env"
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 echo Cleaning in: %CD%
 rmdir /S /Q out
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 @rem ***************************************************************************
 
 cd "%TOP%\examples\web_config"
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 echo Cleaning in: %CD%
 rmdir /S /Q out
-if errorlevel 1 exit /B 1
+if errorlevel 1 (popd; exit /B 1)
 
 @rem ***************************************************************************
 
 echo Completed
+popd
 exit /B 0
 
 @rem ***************************************************************************
