27a9ad810637c6cca2193b6392c61f08c9d106cb
aiurovet <aiurovet@gmail.com>
2023-05-18 23:48:56 +1000

    Added build.options

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -3,107 +3,194 @@
 # Copyright (c) 2023 Alexander Iurovetski
 ################################################################################
 
-param(
-  [switch] $ChocoOnly = $false,
-  [switch] $IconsOnly = $false
-)
-
-$ErrorActionPreference = "Stop"
-
 $ProjectName = "xnx"
 $ProjectVer = "0.2.0"
-$CanBuild = !$ChocoOnly -and !$IconsOnly
+$StartupDir = Get-Location
 
-##############################################################################
-# Initialising paths
-##############################################################################
+################################################################################
+# This script info
+################################################################################
 
 $ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptBaseName = [IO.Path]::GetFileName($ScriptPath)
 $ProjectDir = (Get-Item $ScriptPath).Directory.Parent.FullName
 
-$AppDir = "$ProjectDir\app\Windows"
-$BinDir = "$ProjectDir\bin\Windows"
-$ChocoDir = "$ProjectDir\scripts\install\choco"
-$OutParentDir = "$ProjectDir\out\Windows"
-$OutDir = "$OutParentDir\$ProjectName"
+################################################################################
+# Global variables (compatible with PowerShell v5)
+################################################################################
 
-$ExePath = "$BinDir\$ProjectName.exe"
+$IsOSWindows = if ($Env:USERPROFILE) {$true} else {$false}
+$IsOSMacOS   = if (!$IsOSWindows -and (Test-Path "/Applications" -PathType Container)) {$true} else {$false}
+$IsOSLinux   = if (!$IsOSWindows -and !$IsOSMacOS -and $Env:HOME) {$true} else {$false}
+$LineBreak   = if ($IsOSWindows) {"`r`n"} else {"`n"}
+
+$OSName      = if ($IsOSLinux) {"Linux"} `
+               else {if ($IsOSMacOS) {"macOS"} `
+               else {if ($IsOSWindows) {"Windows"} `
+               else {$null}}}
+
+$AppDir = [IO.Path]::Combine($ProjectDir, "app", $OSName)
+$BinDir = [IO.Path]::Combine($ProjectDir, "bin", $OSName)
+$ChocoDir = [IO.Path]::Combine($ProjectDir, "scripts", "install", "choco")
+$ExePath = [IO.Path]::Combine($BinDir, "$ProjectName.exe")
+$OutParentDir = [IO.Path]::Combine($ProjectDir, "out", $OSName)
+$OutDir = [IO.Path]::Combine($OutParentDir, $ProjectName)
+
+$Yes = "Yes"
+$No = "No"
+
+$global:OptBrew = $false
+$global:OptChoco = $false
+$global:OptCompile = $false
 
 ################################################################################
 # The entry point
 ################################################################################
 
 function AppRun() {
-  Write-Output "Switching to the project's top directory to `"$ProjectDir`""
-  Set-Location -Path $ProjectDir
+  try {
+    Set-Location $ProjectDir
+    Write-Output "Running the build for $OSName with $(LoadOptions)"
 
-  Write-Output "Running the build for Windows"
+    CreateDirIfNotExists $AppDir "application"
 
-  CreateDirIfNotExists $AppDir "application"
+    if ($global:OptCompile -or $global:OptIcons) {
+      CreateDirIfNotExists $BinDir "bin"
+      CreateDirIfNotExists $OutDir "output"
+    }
 
-  if (!$ChocoOnly) {
-    CreateDirIfNotExists $BinDir "bin"
-    CreateDirIfNotExists $OutDir "output"
+    if ($global:OptCompile) {
+      Write-Output "Getting the latest version of the packages"
+      RunCommand "dart" @("pub", "get")
+
+      Write-Output "Compiling $ProjectName $ProjectVer"
+      $Script = [IO.Path]::Combine($ProjectDir, "bin", "main.dart")
+      RunCommand "dart" @("compile", "exe", $Script, "-o", $ExePath)
+    }
+
+    if ($global:OptIcons) {
+      Write-Output "Creating the icons in the output directory"
+      $Script = [IO.Path]::Combine($ProjectDir, "scripts", "mkicons")
+      RunCommand $ExePath @("-d", $Script, $ProjectName, $OutDir)
+    }
+
+    if ($global:OptChoco) {
+      Write-Output "Replacing hash in the verification file"
+      ReplaceHash
+
+      Write-Output "Creating the Chocolatey package in `"$AppDir`""
+      $NuspecPath = [IO.Path]::Combine($ChocoDir, "$ProjectName.nuspec")
+      RunCommand "choco" @("pack", $NuspecPath, "--outdir", $AppDir)
+    }
+
+    Write-Output "The build successfully completed"
+  } catch {
+    Write-Output "The build failed"
+    Exit 1
+  } finally {
+    CleanUp
   }
+}
 
-  if ($CanBuild) {
-    Write-Output "Getting the latest version of the packages"
-    RunCommand "dart" @("pub", "get")
+################################################################################
 
-    Write-Output "Compiling $ProjectName $ProjectVer"
-    RunCommand "dart" @("compile", "exe", "bin\main.dart", "-o", $ExePath)
-  }
-
-  if ($IconsOnly) {
-    Write-Output "Creating the icons in the output directory"
-    RunCommand $ExePath @("-d", "scripts\mkicons", $ProjectName, $OutDir)
-  }
-
-  if ($ChocoOnly) {
-    Write-Output "Replacing hash in the verification file"
-    ReplaceHash
-
-    Write-Output "Creating the Chocolatey package in `"$AppDir`""
-    $NuspecPath = "$ChocoDir\$ProjectName.nuspec"
-    RunCommand "choco" @("pack", $NuspecPath, "--outdir", $AppDir)
-  }
-  else {
+function CleanUp {
+  if (Test-Path $OutParentDir -PathType Container) {
     Write-Output "Deleting the output parent directory `"$OutParentDir`""
     Remove-Item $OutParentDir -Recurse
   }
 
-  Write-Output "The build successfully completed"
+  Set-Location $StartupDir
 }
 
 ################################################################################
 
 function CreateDirIfNotExists {
-  param ([String]$Dir, [String]$Hint)
+  param ([string]$Dir, [string]$Hint)
   
-  if (Test-Path -Path $Dir) {
+  if (Test-Path $Dir -PathType Container) {
     return
   }
 
+  $LASTEXITCODE = 0
+
   Write-Output "Creating the $Hint directory `"$Dir`""
   New-Item $Dir -ItemType "Directory" | Out-Null
+
+  if ($LASTEXITCODE -ne 0) {
+    throw
+  }
+}
+
+################################################################################
+
+function LoadOptions {
+  $oldExt = [IO.Path]::GetExtension($ScriptBaseName)
+  $newExt = ".options"
+
+  if ($oldExt) {
+    $optPath = [IO.Path]::ChangeExtension($ScriptPath, $NewExt)
+  } else {
+    $optPath = "$ScriptPath$NewExt"
+  }
+
+  $text = Get-Content $optPath -Raw
+  $text = $LineBreak + $text.Replace("[ `t]+", "").ToLower() + $LineBreak
+
+  $global:OptBrew = $text.Contains("$LineBreak-brew$LineBreak")
+  $global:OptChoco = $text.Contains("$LineBreak-choco$LineBreak" )
+  $global:OptCompile = $text.Contains("$LineBreak-compile$LineBreak")
+  $global:OptIcons = $text.Contains("$LineBreak-icons$LineBreak")
+
+  $optStr = ""
+  $optStr += ", compile=$(if ($global:OptCompile) {$Yes} else {$No})"
+  $optStr += ", icons=$(if ($global:OptIcons) {$Yes} else {$No})"
+  $optStr += ", brew=$(if ($global:OptBrew) {$Yes} else {$No})"
+  $optStr += ", choco=$(if ($global:OptChoco) {$Yes} else {$No})"
+  if (!$optStr) {$optStr += "<none>"}
+
+  return $optStr.Substring(2)
 }
 
 ################################################################################
 
 function ReplaceHash {
+  Write-Output "Getting hash for $ExePath"
   $Hash = Get-FileHash -Path $ExePath -Algorithm SHA256 | ForEach-Object {$_.Hash}
-  $File = "$ChocoDir\tools\VERIFICATION.txt"
+
+  if ($LASTEXITCODE -ne 0) {
+    throw
+  }
+
+  $File = [IO.Path]::Combine($ChocoDir, "tools", "VERIFICATION.txt")
+  Write-Output "Loading file `"$File`""
+
   $Text = Get-Content -Path $File
+
+  if ($LASTEXITCODE -ne 0) {
+    throw
+  }
+
   $Text = $Text -replace "(\s*x64\s*Checksum:\s*)[0-9A-Fa-f]+(.*)", "`${1}$Hash`${2}"
+
+  Write-Output "Updating file `"$File`""
   Set-Content -Path $File -Value $Text
+
+  if ($LASTEXITCODE -ne 0) {
+    throw
+  }
 }
 
 ################################################################################
 
 function RunCommand {
-  param ([String]$CmdPath, [String[]]$CmdArgs)
+  param ([string]$CmdPath, [string[]]$CmdArgs)
   
   & $CmdPath $CmdArgs
+
+  if ($LASTEXITCODE -ne 0) {
+    throw
+  }
 }
 
 ################################################################################
--- scripts/install/choco/tools/chocolateyinstall.ps1
+++ scripts/install/choco/tools/chocolateyinstall.ps1
@@ -1,15 +1,10 @@
-﻿# Get-FileHash -Algorithm SHA256 -Path "$env:USERPROFILE\Projects\choco\xnx\*.zip"
-
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName    = 'xnx'
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   softwareName   = 'xnx*'
-  #url64bit       = 'https://github.com/aiurovet/xnx/raw/0.1.0/app/Windows/xnx-0.1.0-windows-x86_64.zip'
-  #checksum64     = 'c0426374b7722d7ee3e33a757c080248e38ebfb79553b3043eb85d16386f8b53'
-  #checksumType64 = 'sha256' # certutil -hashfile <file> SHA256
 }
 
 #Invoke-WebRequest -URI $packageArgs.url64bit
