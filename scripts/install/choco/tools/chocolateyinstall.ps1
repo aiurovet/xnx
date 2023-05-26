713ac7169942282ae8e78dcadc7bc9bb033eef0b
aiurovet <aiurovet@gmail.com>
2023-05-26 18:11:28 +1000

    WIP

--- scripts/install/choco/tools/chocolateyinstall.ps1
+++ scripts/install/choco/tools/chocolateyinstall.ps1
@@ -8,5 +8,5 @@ Get-ChocolateyWebFile `
     -PackageName $app `
     -FileFullPath "$toolsDir\$app.exe" `
     -Url64bit "https://github.com/aiurovet/$app/raw/0.2.0/bin/Windows/$app.exe" `
-    -Checksum64 "007D48B77E794CF6D5DC79053D9E99A02B2F888897AC7A500265CA92414BF71B" `
+    -Checksum64 "E6835D5B05B94C4A5C6EA430CA551403E3F12B403F7493F8A1ED584F9A06B799" `
     -ChecksumType64 "SHA256"
